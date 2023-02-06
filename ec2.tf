3716f107c8e1705e69e5c3badfe2a3f1413347ae
Dieudonne Ndenge <ndengued@gmail.com>
2023-02-06 16:38:09 -0600

    modifications on ec2 code

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "demo" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  monitoring = true
+  ebs_optimized = true
   tags = {
     Name =var.ec2_name_tag
   }
