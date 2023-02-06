568d9d0b860097013b686c4917600cb5350bb554
Dieudonne Ndenge <ndengued@gmail.com>
2023-02-06 16:53:14 -0600

    corections made on checkov scan

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,10 @@ resource "aws_instance" "demo" {
   instance_type = var.instance_type
   monitoring = true
   ebs_optimized = true
+  root_block_device {
+    encrypted = true
+  }
+  iam_instance_profile = "test"
   tags = {
     Name =var.ec2_name_tag
   }
