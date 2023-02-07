4241f1b48edca522d4f9d29b2e92c8ddeab21134
Dieudonne Ndenge <ndengued@gmail.com>
2023-02-07 01:07:21 -0600

    added new resource

--- s3.tf
+++ s3.tf
@@ -1,6 +1,12 @@
-resource "aws_s3_bucket" "bucket" {
+# resource "aws_s3_bucket" "bucket" {
 
-  tags = {
-    Name = "buckus"
-  }
-}
+#   block_public_acls   = true
+#   block_public_policy = true
+#   replication_configuration {
+    
+#   }
+
+#   tags = {
+#     Name = "buckus"
+#   }
+# }
