296ddad8fc102819f0c6515bd80c3718cb8cf597
Dieudonne Ndenge <ndengued@gmail.com>
2023-02-07 00:49:01 -0600

    initiating changes

--- var.tf
+++ var.tf
@@ -2,7 +2,7 @@ variable "ami_id" {
   default = "ami-0aa7d40eeae50c9a9"
 }
 variable "instance_type" {
-  default = "t2.medium"
+  default = "t3.micro"
 }
 variable "ec2_name_tag" {
   default = "trigger"
